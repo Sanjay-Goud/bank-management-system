14c96d6e8ab9440fce0b9cf2dcd6fba3a5d9b2fa
Sanjay Veeramalla <veeramallasanjaygoud@gmail.com>
2025-10-10 18:33:56 +0530

    Updated index.html

--- src/main/java/com/sanjay/bms/config/SecurityConfig.java
+++ src/main/java/com/sanjay/bms/config/SecurityConfig.java
@@ -128,7 +128,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:8080",
                 "http://127.0.0.1:8080",
-                "http://localhost:5500"
+                "http://localhost:5500",
+                "https://banking-management-system-w35p.onrender.com"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
